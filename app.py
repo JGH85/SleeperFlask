ab0cbfabe77bcdfc09c7fedb378cb2e2c169bd95
Joseph Houser <hjoseph@wustl.edu>
2022-12-07 15:04:15 -0600

    fixed app.py

--- app.py
+++ app.py
@@ -740,9 +740,10 @@ class Comments(db.Model):
 
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=3000)
+    # db.create_all()
+    # app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=3000)
 
 
 #features to add in future:
